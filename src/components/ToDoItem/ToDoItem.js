ef6f9aa6277bb0c74ae30079b3dc309c081b738a
johnleekj <johnleekaijie@gmail.com>
2020-06-07 11:30:06 +0800

    phase 7

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -3,12 +3,17 @@ import React from 'react';
 class ToDoItem extends React.Component {
     
     render() {
+        const completedStyle = {
+            fontstyle: "italic",
+            color: "#cdcdcd",
+            textDecoration: "line-through"
+        }
         return (
             <div className="todo-item" >
                 <input type="checkbox" 
                         onChange={() => this.props.handleChange(this.props.item.id)} 
                         checked={this.props.item.completed} />
-                <p>{this.props.item.text}</p>
+                <p style={this.props.item.completed ? completedStyle : null}>{this.props.item.text}</p>
             </div>       
         )
     }
